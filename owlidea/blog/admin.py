8af0f50f8337a1eec81e695ba6feffa6fb5bcfb9
owl <owlhub@163.com>
2022-12-06 11:16:58 +0800

    抽象类

--- owlidea/blog/admin.py
+++ owlidea/blog/admin.py
@@ -1,12 +1,16 @@
 from django.contrib import admin
 from django.urls import reverse
 from django.utils.html import format_html
+from django.contrib.admin.models import LogEntry
+
+
 
 from .adminforms import PostAdminForm
 from .models import Tag, Post, Category
 
 # Register your models here.
 from owlidea.custom_site import custom_site
+from owlidea.base_admin import BaseOwnerAdmin
 
 
 class PostInline(admin.TabularInline):
@@ -16,14 +20,10 @@ class PostInline(admin.TabularInline):
 
 
 @admin.register(Category, site=custom_site)
-class CategoryAdmin(admin.ModelAdmin):
+class CategoryAdmin(BaseOwnerAdmin):
     inlines = [PostInline,]
     list_display = ('name', 'status', 'is_nav','create_time','post_count')
     fields = ('name', 'status', 'is_nav')
-
-    def save_model(self, request, obj, form, change):
-        obj.owner = request.user
-        return super(CategoryAdmin, self).save_model(request, obj, form, change)
 
     def post_count(self,obj):
         return obj.post_set.count()
@@ -31,13 +31,9 @@ class CategoryAdmin(admin.ModelAdmin):
 
 
 @admin.register(Tag, site=custom_site)
-class TagAdmin(admin.ModelAdmin):
+class TagAdmin(BaseOwnerAdmin):
     list_display = ('name','status', 'create_time')
     fields = ('name', 'status')
-
-    def save_model(self, request, obj, form, change):
-        obj.owner = request.user
-        return super(TagAdmin, self).save_model(request, obj, form, change)
 
 
 class CategoryOwnerFilter(admin.SimpleListFilter):
@@ -55,11 +51,11 @@ class CategoryOwnerFilter(admin.SimpleListFilter):
 
 
 @admin.register(Post, site=custom_site)
-class PostAdmin(admin.ModelAdmin):
+class PostAdmin(BaseOwnerAdmin):
     form = PostAdminForm
     list_display = ('title', 'category', 'status', 'create_time', 'owner', 'operator')
     list_display_link = []
-    list_filter = [CategoryOwnerFilter,'category']
+    list_filter = [CategoryOwnerFilter]
     search_fields = ['title', 'category__name']
 
     actions_on_top = True
@@ -107,16 +103,13 @@ class PostAdmin(admin.ModelAdmin):
         )
     operator.short_description = '操作'
 
-    def save_model(self, request, obj, form, change):
-        obj.owner = request.user
-        return super(PostAdmin, self).save_model(request, obj, form, change)
-
-    def get_queryset(self,request):
-        qs = super(PostAdmin, self).get_queryset(request)
-        return qs.filter(owner=request.user)
-
     class Media:
         css = {
             'all': ("https://cdn.bootcss.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css",),}
         js = ("https://cdn.bootcss.com/bootsrap/4.0.0-beta.2/js/bootstrap.min.js",)
 
+
+# 记录日志
+@admin.register(LogEntry,site=custom_site)
+class LogEntryAdmin(admin.ModelAdmin):
+    list_display = ('object_repr', 'object_id', 'action_flag','user','change_message')
--- owlidea/config/admin.py
+++ owlidea/config/admin.py
@@ -1,25 +1,21 @@
 from django.contrib import admin
 from .models import Link,Sidebar
 from owlidea.custom_site import custom_site
+from owlidea.base_admin import BaseOwnerAdmin
 
 # Register your models here.
 
+
 @admin.register(Link, site=custom_site)
-class LinkAdmin(admin.ModelAdmin):
+class LinkAdmin(BaseOwnerAdmin):
     list_display = ('title', 'href','status', 'weight', 'create_time')
     fields = ('title', 'href', 'status', 'weight')
 
-    def save_model(self, request, obj, form, change):
-        obj.owner = request.user
-        return super(Link).save_model(request, obj, form, change)
 
 
 @admin.register(Sidebar, site=custom_site)
-class SidebarAdmin(admin.ModelAdmin):
+class SidebarAdmin(BaseOwnerAdmin):
     list_display = ('title', 'display_type','content', 'status', 'create_time')
     fields = ('title', 'display_type', 'content')
 
-    def save_model(self, request, obj, form, change):
-        obj.owner = request.user
-        return super(Sidebar).save_model(request, obj, form, change)
 
